c10a1890fb2d182a2ad2ba1b34c7a55cca20dd44
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-10-25 20:51:18 -0300

    Extrair função para definir cores de um palpite
    Esta refatoração extraiu uma função de dentro de "play" para computar as cores que devem ser impressas para um palpite feito pelo usuário. Além de tornar o código mais legível, essa refatoração ajudou a diminuir a complexidade ciclomática da função "play".

--- game.py
+++ game.py
@@ -87,6 +87,29 @@ def init_color_dict():
     return {letter: consts.FBWHITE for letter in consts.LETTERS}
 
 
+def compute_guess_colors(guessed_word, secret_word, count_dict, color_dict, guess_display):
+    uncolored_indexes = []
+
+    for (index, char) in enumerate(guessed_word):
+        if char == secret_word[index]:
+            guess_display[index] = consts.BGREEN + char + consts.CEND
+            color_dict.update({char: consts.FBGREEN})
+            count_dict[char] -= 1
+        else:
+            uncolored_indexes.append(index)
+
+    for index in uncolored_indexes:
+        char = guessed_word[index]
+        if char in count_dict and count_dict.get(char) > 0:
+            guess_display[index] = consts.BYELLOW + char + consts.CEND
+            if color_dict[char] != consts.FBGREEN:
+                color_dict.update({char: consts.FBYELLOW})
+            count_dict[char] -= 1
+        else:
+            guess_display[index] = consts.BWHITE + char + consts.CEND
+            color_dict.update({char: consts.FBLACK})
+
+
 def play(word_size, file_name, is_daily=False):
     max_tries = word_size + 1
     if is_daily:
@@ -119,26 +142,9 @@ def play(word_size, file_name, is_daily=False):
 
         print()
 
-        uncolored_indexes = []
         count_dict = get_count_dict(secret_word)
-        for (index, char) in enumerate(guessed_word):
-            if char == secret_word[index]:
-                guesses[tries][index] = consts.BGREEN + char + consts.CEND
-                color_dict.update({char: consts.FBGREEN})
-                count_dict[char] -= 1
-            else:
-                uncolored_indexes.append(index)
 
-        for index in uncolored_indexes:
-            char = guessed_word[index]
-            if char in count_dict and count_dict.get(char) > 0:
-                guesses[tries][index] = consts.BYELLOW + char + consts.CEND
-                if color_dict[char] != consts.FBGREEN:
-                    color_dict.update({char: consts.FBYELLOW})
-                count_dict[char] -= 1
-            else:
-                guesses[tries][index] = consts.BWHITE + char + consts.CEND
-                color_dict.update({char: consts.FBLACK})
+        compute_guess_colors(guessed_word, secret_word, count_dict, color_dict, guesses[tries])
 
         tries += 1
 
